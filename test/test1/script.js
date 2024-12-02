1e5baf37a6c0010cc4ed6362381b3c3d9fc134b5
Speltorsk <kasper.hallen.edu@rattvik.se>
2024-12-02 13:47:20 +0100

    update

---
+++ test/test1/script.js
@@ -0,0 +1,16 @@
+const button = document.getElementById('addContentButton');
+const contentArea = document.getElementById('contentArea')
+const outputText = document.getElementsByid('outputText')
+
+//button.addEventListener('click', () => {
+
+  //  const newParagraph = document.createElement('P');
+    //newParagraph.innerHTML = 'hej detta är ett test';
+
+    //contentArea.appendChild(newParagraph);
+
+//});
+
+//button.addEventListener('click', (InputEvent) =>{
+
+//});
