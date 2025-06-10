2b5001b7361392f79baf429aece94878d3437dca
Speltorsk <kasper.hallen.edu@rattvik.se>
2025-06-10 12:53:46 +0200

    fw

--- Fina-sidan-2/Fina-sidan/script.js
+++ Fina-sidan-2/Fina-sidan/script.js
@@ -1,20 +1,4 @@
-// Add event listeners to buttons for hover and click sounds
-const buttons = document.querySelectorAll('.fancy-button');
 
-buttons.forEach(button => {
-    const hoverSound = new Audio(button.dataset.hoverSound);
-    const clickSound = new Audio(button.dataset.clickSound);
-
-    button.addEventListener('mouseenter', () => {
-        hoverSound.currentTime = 0; // Reset sound
-        hoverSound.play();
-    });
-
-    button.addEventListener('click', () => {
-        clickSound.currentTime = 0; // Reset sound
-        clickSound.play();
-    });
-});
 
 
 const banner = document.getElementById('scrollBanner');
